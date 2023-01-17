feeeed500eb4d697557d51f166ee68d8e393bfcf
Syahiran <muhammad-syahiran.aina-radzman@keysight.com>
2023-01-17 09:32:47 +0800

    update the message, to test poll scm

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I love Jenkins!!");
+        System.out.println("2nd Day of Jenkins training!!");
     }
 
     public String getMessage() {
